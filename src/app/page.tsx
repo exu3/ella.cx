3a479d96a34b8be6d5cea295776c072f1e35f3c9
exu <git@ella.cx>
2025-05-25 09:56:20 -0400

    words

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,11 +9,9 @@ export default function Home() {
         <p>Ella is a maker of things, based somewhere in Vermont.</p>
         <p>Currently studying engineering at the University of Vermont.</p>
         <p>
-          Experienced in sustainable trail building, carpentry, growing
-          veg/fruit/poultry, web programming, and fabrication.
+          Interest and experience in trail building, carpentry, agriculture,
+          energy, web programming, and fabrication.
         </p>
-        {/*  <br />
-        <p>Employable for summer 2025.</p>*/}
       </div>
       <div>
         <PhotoGallery
