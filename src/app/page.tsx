bc56a11061001b94287360357459802fbbe70448
exu <git@ella.cx>
2025-09-06 14:18:29 -0400

    another copy refresh

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,15 +11,36 @@ export default function Home() {
       <div className={styles.header}>
         <p>
           I make home in Huntington, VT and do work that engages with societal
-          functions in a way that is tangible, fun/interesting, and useful
-          (sometimes in unconventional ways). I am currently on a side quest as
-          an undergrad in the College of Engineering at UVM.
+          functions in ways that are tangible, useful, and often a bit
+          unconventional.
         </p>
         <br />
         <p>
-          Have experience in carpentry, fabrication, agriculture, energy,
-          software development, design, and trail building.
+          I have experience in carpentry, fabrication, agriculture, energy, and
+          software development, and I am currently on a side quest in
+          Engineering School in pursuit of a college degree.
         </p>
+        <br />
+        <p>
+          You can reach me by electronic mail addressed to me at this domain. Or
+          by calling my landline, the digits of which can be derived by
+          integration of the following:
+        </p>
+        <br />
+        <div
+          style={{
+            backgroundColor: "white",
+            padding: "12px",
+            width: "400px",
+          }}
+        >
+          <Image
+            alt="integral which computes digits of phone number"
+            src="/landline-integral.png"
+            width="342"
+            height="53"
+          />
+        </div>
       </div>
       <div>
         <PhotoGallery
